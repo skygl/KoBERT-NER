d118b254f206f601f876f2c8bb47981381d6a4cd
skygl <jupiter9501@gmail.com>
2022-06-09 21:09:03 +0900

    fix: 모델 업데이트 부분 수정

--- trainer.py
+++ trainer.py
@@ -40,7 +40,7 @@ class Trainer(object):
                 self.model = self.model_class.from_pretrained(args.model_name_or_path, config=self.config)
             else:
                 self.model_class = BertNER
-                self.model = self.model_class.from_pretrained(args.model_dir,
+                self.model = self.model_class.from_pretrained(args.model_name_or_path,
                                                               dataset_label_nums=[len(self.label_lst)],
                                                               output_attentions=False,
                                                               output_hidden_states=False)
@@ -233,7 +233,20 @@ class Trainer(object):
             raise Exception("Model doesn't exists! Train first!")
 
         try:
-            self.model = self.model_class.from_pretrained(self.args.model_dir)
+            if self.args.task == 'naver-ner':
+                self.model = self.model_class.from_pretrained(self.args.model_dir)
+            else:
+                state = torch.load(self.args.model_dir)
+                self.model = self.model_class.from_pretrained(self.args.model_name_or_path,
+                                                              dataset_label_nums=[len(self.label_lst)],
+                                                              output_attentions=False,
+                                                              output_hidden_states=False)
+                pretrained_dict = state.state_dict()
+                model_dict = self.model.state_dict()
+                pretrained_dict = {k: v for k, v in pretrained_dict.items() if
+                                   k in model_dict and 'classifiers.0.' not in k}
+                model_dict.update(pretrained_dict)
+                self.model.load_state_dict(model_dict)
             self.model.to(self.device)
             logger.info("***** Model Loaded *****")
         except:
